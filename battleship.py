59e09632fd7fac21dfc9b55ae67d54fd90c35862
abobof09 <alden.bobofchak@icloud.com>
2025-04-19 22:39:05 -0400

    started writing the __init__ method for the Game class

--- battleship.py
+++ battleship.py
@@ -1,10 +1,28 @@
-from random import randint
+from random import randint # Will allow randomized ship placements
 import os
 
-class Ship:
-    def __init__(self):
-        pass
-
 class Game:
-    def __init__(self):
-        pass
+    def __init__(self, size, direction, location):
+        """
+        Initialize the Game class with logic attributes.
+        Args:
+            size: the size of the ship(s)
+            direction: the direction the ship(s) are facing
+            location: the starting coordinate of the ship(s)
+        """
+        self.size = size
+        if direction.lower() == "horizontal" or direction.lower() == "vertical":
+            self.direction = direction
+        else:
+            raise ValueError("Value should be either horizontal or vertical.")
+        
+        # Properly orient the ships based on the parameters
+        if direction.lower() == "horizontal":
+            if location['row'] in range(row_size):
+                # List to store the coordinates?
+                self.coordinate = []
+                for x in range(size):
+                    if location['col'] + x in range(col_size):
+                        self.coordinate.append({'row': location['row'], 'col': location['col'] + x})
+                    else:
+                        raise IndexError("The column does not exist.")
