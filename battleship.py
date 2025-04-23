5e83b30752c878e75f619f3ffce6de903c2d1981
abobof09 <alden.bobofchak@icloud.com>
2025-04-23 10:19:10 -0400

    continued game logic, working on vertical ships

--- battleship.py
+++ battleship.py
@@ -18,11 +18,24 @@ class Game:
         
         # Properly orient the ships based on the parameters
         if direction.lower() == "horizontal":
-            if location['row'] in range(row_size):
+            if location['row'] in range(num_cols):
                 # List to store the coordinates?
-                self.coordinate = []
+                self.coords = []
                 for x in range(size):
-                    if location['col'] + x in range(col_size):
-                        self.coordinate.append({'row': location['row'], 'col': location['col'] + x})
+                    if location['col'] + x in range(num_rows):
+                        self.coords.append({'row': location['row'], 'col': location['col'] + x})
                     else:
                         raise IndexError("The column does not exist.")
+                else:
+                    raise IndexError("The row does not exist.")
+        elif direction.lower() == "vertical":
+            if location['col'] in num_rows:
+                self.coords = []
+                for x in range(size):
+                    if location['row'] + x in num_cols
+
+
+
+# to avoid errors (size of the board):
+num_cols = 10
+num_rows = 10
