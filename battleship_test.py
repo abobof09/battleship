aa042420554675200f811b7c3f5e23cd99be682f
abobof09 <alden.bobofchak@icloud.com>
2025-05-05 10:26:53 -0400

    implemented setUp and test_horizontal_ship_creation

---
+++ battleship_test.py
@@ -0,0 +1,22 @@
+import unittest
+from battleship import Game, Scoreboard
+
+class TestMethods(unittest.TestCase):
+    def setUp(self):
+        """
+        Sets a fresh board up for each individual test.
+        """
+        global board, board_display, num_rows, num_cols
+        num_rows = 9
+        num_cols = 9
+        board = [[0 for _ in range(num_rows)] for _ in range(num_cols)]
+        board_display = [["O" for _ in range(num_rows)] for _ in range(num_cols)]
+
+    def test_horizontal_ship_creation(self):
+        ship = Game(3, 'horizontal', {'row': 2, 'col': 1})
+        expected_coords = [
+            {'row': 2, 'col': 1},
+            {'row': 2, 'col': 2},
+            {'row': 2, 'col': 3}
+        ]
+        self.assertEqual(ship.coords, expected_coords)
